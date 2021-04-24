ab035367be35897e2036fb6e6e233c81fdffb19c
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-04-24 20:31:28 +0900

    sign upページ作成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  def choose_url
+    if action_name == 'new'
+      users_path
+    elsif action_name == 'edit'
+      user_path
+    end
+  end
+  def choose_method
+    if action_name == 'new'
+      'post'
+    elsif action_name == 'edit'
+      'patch'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
     resources :users, only: [:new, :create, :show]
     resources :sessions, only: [:new, :create, :destroy]
     resources :picture_blogs do
-   collection do
-    post :confirm
-   end
+    collection do
+      post :confirm
+    end
   end
 end
