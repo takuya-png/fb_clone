2ac590d6f6741a00a83a19e339d43074afa0ccd4
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-04-24 20:45:43 +0900

    profileページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
   def new
     @user = User.new
   end
 
   def show
+  end
+  
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
