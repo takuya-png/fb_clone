f40895119f918758f071d2dece1fdae3ef65e56c
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-05-03 12:04:47 +0900

    ユーザコントローラ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,42 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
-  def show
-  end
-  
   def create
     @user = User.new(user_params)
-    if params[:back]
-      render :new
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
     else
-      if @user.save
-        redirect_to user_path(@user.id), notice: "ユーザー登録しました"
-      else
-        render :new
-      end
+      render :new
     end
   end
 
   def edit
-  end
-
-  def update
-    if params[:back]
-      render :edit
-    else
-      if @user.update(user_params)
-        redirect_to user_path(@user.id), notice: "編集しました"
-      else
-        render :edit
-      end
-    end
-  end
-
-
-  private
-
-  def set_user
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールを更新しました"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
   end
 end
