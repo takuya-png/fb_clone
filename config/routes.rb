5d0ac9ceb17b64aea866b5689b530803278f8918
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-05-03 14:06:43 +0900

    ルータ修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :new, :create, :edit, :update, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  root to: 'sessions#new'
   resources :picture_blogs do
     collection do
       post :confirm
